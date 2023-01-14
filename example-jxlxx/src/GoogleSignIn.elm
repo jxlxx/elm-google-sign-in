e65b20ca630df3235534a5755a60a2e6872bd83b
jxlxx <jul14+git@proton.me>
2023-01-14 12:29:49 -0500

    have to work from examples dir for now, works

---
+++ example-jxlxx/src/GoogleSignIn.elm
@@ -0,0 +1,157 @@
+module GoogleSignIn exposing
+    ( view, styledView
+    , Attribute
+    , idAttr
+    , onSignIn
+    , Profile
+    , ClientId(..), encodeId
+    )
+
+{-| Elm bindings to the "Sign in With Google" widget
+
+See the github for more information: <https://github.com/cedric-h/elm-google-sign-in>
+
+
+## View
+
+@docs view, styledView
+
+
+## Attribute Wrapper
+
+@docs Attribute
+
+
+## Properties
+
+@docs idAttr
+
+
+## Listeners
+
+@docs onSignIn
+
+
+## Profile
+
+@docs Profile
+
+
+## ClientId & co.
+
+@docs ClientId, encodeId
+
+-}
+
+import Html as PlainHtml
+import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes as Attributes exposing (property)
+import Html.Styled.Events as Events exposing (on)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode exposing (Value)
+
+
+
+-- ATTRIBUTE WRAPPER
+
+
+{-| Like a normal HTML Attribute, but these can only apply to Google Sign in Buttons
+-}
+type Attribute msg
+    = Attr (Html.Attribute msg)
+
+
+unattr : Attribute msg -> Html.Attribute msg
+unattr (Attr a) =
+    a
+
+
+
+-- PROPERTIES
+
+
+{-| What Google uses to keep track of your application
+-}
+type ClientId
+    = Id String
+
+
+{-| Turns a ClientId into a JSON value,
+mainly for shipping through a port.
+-}
+encodeId : ClientId -> Value
+encodeId (Id id) =
+    Encode.string id
+
+
+{-| Supply the ClientId for the application this button should sign in to.
+-}
+idAttr : ClientId -> Attribute msg
+idAttr =
+    encodeId >> property "clientId" >> Attr
+
+
+
+-- LISTENERS
+
+
+{-| Respond to when the user completes signing in through Google.
+-}
+onSignIn : (Profile -> msg) -> Attribute msg
+onSignIn tagger =
+    Decode.at [ "target", "profile" ] profileDecoder
+        |> Decode.map tagger
+        |> on "signIn"
+        |> Attr
+
+
+
+-- PROFILE
+
+
+{-| All of the important information Google stores about a user.
+The `idToken` is what should be sent back to your server for authentication purposes.
+The `email` field is only present if your clientId has the right scopes.
+-}
+type alias Profile =
+    { id : String
+    , idToken : String
+    , name : String
+    , givenName : String
+    , familyName : String
+    , imageUrl : String
+    , email : Maybe String
+    }
+
+
+profileDecoder : Decoder Profile
+profileDecoder =
+    Decode.map7
+        Profile
+        (Decode.field "id" Decode.string)
+        (Decode.field "idToken" Decode.string)
+        (Decode.field "name" Decode.string)
+        (Decode.field "givenName" Decode.string)
+        (Decode.field "familyName" Decode.string)
+        (Decode.field "imageUrl" Decode.string)
+        (Decode.field "email" (Decode.nullable Decode.string))
+
+
+
+-- VIEW
+
+
+{-| Yields a Google sign in button
+intended for use with rtfeldman/elm-css
+-}
+styledView : List (Attribute msg) -> Html msg
+styledView attributes =
+    Html.node "google-signin-button" (List.map unattr attributes) []
+
+
+{-| Yields a Google sign in button
+intended for use with elm/html
+-}
+view : List (Attribute msg) -> PlainHtml.Html msg
+view attributes =
+    Html.toUnstyled <| styledView attributes
---
+++ example-jxlxx/src/Main.elm
@@ -0,0 +1,89 @@
+port module Main exposing (..)
+
+import Browser
+import GoogleSignIn
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Json.Encode as E
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+    
+    
+id : GoogleSignIn.ClientId
+id = GoogleSignIn.Id "789723491626-vf2tt6ei0fue08ajqjtr9k3nkqfe1pn2"
+   
+port googleSignOut : E.Value -> Cmd msg
+port googleSignOutComplete : (E.Value -> msg) -> Sub msg
+   
+   
+   
+-- MODEL
+type alias Model =
+    Maybe String
+  
+  
+init : () -> ( Model, Cmd msg )
+init () = ( Nothing, Cmd.none )
+ 
+ 
+ 
+-- UPDATE
+ 
+ 
+type Msg
+    = SignIn GoogleSignIn.Profile
+    | BeginSignOut
+    | SignOutComplete
+   
+   
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SignIn profile ->
+            ( Just profile.name, Cmd.none )
+        BeginSignOut ->
+            ( model, googleSignOut <| GoogleSignIn.encodeId id )
+        SignOutComplete ->
+            ( Nothing, Cmd.none )
+  
+  
+  
+-- SUBSCRIPTIONS
+  
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    googleSignOutComplete (\a -> SignOutComplete)
+ 
+ 
+ 
+ -- VIEW
+ 
+ 
+view : Model -> Html Msg
+view model =
+    div []
+    [ case model of
+        Just name ->
+            div []
+                [ div [] [ text ("Welcome, " ++ name) ]
+                , div [] [ button [ onClick BeginSignOut ] [ text "Sign Out" ] ]
+                ]
+        Nothing ->
+            GoogleSignIn.view 
+            [ GoogleSignIn.onSignIn SignIn
+            , GoogleSignIn.idAttr id
+            ]
+    ]
+    
+    
